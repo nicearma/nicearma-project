f1034f5f23545a4f4d6b69323f5a802a97054e2d
Nicolas ARMANDO <nicolas.armando.ext@groupama.com>
2018-04-18 19:05:14 +0200

    Add nicearma-lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {NicearmaLibModule} from "nicearma-lib/dist/nicearma-lib";
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NicearmaLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
