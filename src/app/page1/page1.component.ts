fa3e23d4c45137011d37e27e3ba5624f1328b573
Nicolas Armando <nicolas.armando@zenika.com>
2018-04-23 22:15:00 +0200

    Missing page

---
+++ src/app/page1/page1.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-page1',
+  templateUrl: './page1.component.html'
+})
+export class Page1Component {
+
+  constructor() {
+
+  }
+
+}
